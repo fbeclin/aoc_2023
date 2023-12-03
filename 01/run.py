fa07c2f2f0b751c126d9d959af7d435897339a47
fbeclin <60063337+fbeclin@users.noreply.github.com>
2023-12-03 20:23:53 +0100

    part 2

--- 01/run.py
+++ 01/run.py
@@ -1,3 +1,21 @@
+from operator import concat
+import regex as re
+
+
+digits = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+pattern = str.join("|", concat(list(digits.keys()), list(digits.values())))
+
+
 def getNumberFromFirstAndLastDigit(line: str) -> int:
     first_digit = ""
     last_digit = ""
@@ -12,12 +30,26 @@ def getNumberFromFirstAndLastDigit(line: str) -> int:
     return int(first_digit + last_digit)
 
 
+def getNumberWithRegex(line: str) -> int:
+    matches = re.findall(pattern, line, overlapped=True)
+    number = int(
+        digits.get(matches[0], matches[0]) + digits.get(matches[-1], matches[-1])
+    )
+    return number
+
+
 def run1(filename: str):
     with open(filename) as file:
         print(sum([getNumberFromFirstAndLastDigit(line.rstrip()) for line in file]))
 
 
+def run2(filename: str):
+    with open(filename) as file:
+        print(sum([getNumberWithRegex(line.rstrip()) for line in file]))
+
+
 if __name__ == "__main__":
-    # filename = "sample.txt"
-    filename = "input01.txt"
-    run1(filename)
+    # filename = "sample2.txt"
+    filename = "input.txt"
+    # run1(filename)
+    run2(filename)
