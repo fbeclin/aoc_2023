69ffda58087172b7bcb3680836727f38f52c149f
fbeclin <60063337+fbeclin@users.noreply.github.com>
2023-12-03 18:54:03 +0100

    first day

---
+++ 01/run.py
@@ -0,0 +1,23 @@
+def getNumberFromFirstAndLastDigit(line: str) -> int:
+    first_digit = ""
+    last_digit = ""
+    for letter in line:
+        if str.isdigit(letter):
+            first_digit = letter
+            break
+    for letter in reversed(line):
+        if str.isdigit(letter):
+            last_digit = letter
+            break
+    return int(first_digit + last_digit)
+
+
+def run1(filename: str):
+    with open(filename) as file:
+        print(sum([getNumberFromFirstAndLastDigit(line.rstrip()) for line in file]))
+
+
+if __name__ == "__main__":
+    filename = "sample.txt"
+
+    run1(filename)
