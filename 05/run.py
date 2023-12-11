0fd9ce6f9c429d3e5c3edc326aa8d69e6ac20311
fbeclin <60063337+fbeclin@users.noreply.github.com>
2023-12-11 22:43:57 +0100

    use range for part 2

--- 05/run.py
+++ 05/run.py
@@ -1,4 +1,4 @@
-from enum import Enum
+import datetime
 import regex as re
 
 seed2soil_map = []
@@ -8,6 +8,9 @@ water2light_map = []
 light2temperature_map = []
 temperature2humidity_map = []
 humidity2location_map = []
+
+# part 2
+memo = {}
 
 
 def findNumberInMap(number: int, map: []) -> int:
@@ -25,6 +28,73 @@ def getLocationForSeed(seed: int) -> int:
     temperature = findNumberInMap(light, light2temperature_map)
     humidity = findNumberInMap(temperature, temperature2humidity_map)
     return findNumberInMap(humidity, humidity2location_map)
+
+
+def sortMapBySource(map: []):
+    map.sort(key=lambda x: x[1])
+
+
+def sortMaps():
+    sortMapBySource(seed2soil_map)
+    sortMapBySource(soil2fertilizer_map)
+    sortMapBySource(fertilizer2water_map)
+    sortMapBySource(water2light_map)
+    sortMapBySource(light2temperature_map)
+    sortMapBySource(temperature2humidity_map)
+    sortMapBySource(humidity2location_map)
+
+
+def getNewRange(start: int, end: int, map: []) -> []:
+    for i, m in enumerate(map):
+        print(f"map: {m}")
+        m_max = m[1] + m[2]
+        m_diff = m[0] - m[1]
+        if start < m_max:
+            if start < m[1]:
+                break
+            print(f"{start} < {m_max}")
+            # new_start = start + m_diff
+            if end < m_max:
+                # found new range
+                return [[start + m_diff, end + m_diff]]
+            else:
+                # split range
+                return [[start + m_diff, m_max - 1 + m_diff]] + getNewRange(
+                    m_max, end, map[i + 1 :]
+                )
+
+    return [[start, end]]
+
+
+def getMinLocationForSeedRange(seedRanges: []) -> int:
+    soilRanges = []
+    for range in seedRanges:
+        soilRanges += getNewRange(range[0], range[1], seed2soil_map)
+
+    fertilizerRanges = []
+    for range in soilRanges:
+        fertilizerRanges += getNewRange(range[0], range[1], soil2fertilizer_map)
+
+    waterRanges = []
+    for range in fertilizerRanges:
+        waterRanges += getNewRange(range[0], range[1], fertilizer2water_map)
+
+    lightRanges = []
+    for range in waterRanges:
+        lightRanges += getNewRange(range[0], range[1], water2light_map)
+
+    tempRanges = []
+    for range in lightRanges:
+        tempRanges += getNewRange(range[0], range[1], light2temperature_map)
+
+    humidityRanges = []
+    for range in tempRanges:
+        humidityRanges += getNewRange(range[0], range[1], temperature2humidity_map)
+
+    locationRanges = []
+    for range in humidityRanges:
+        locationRanges += getNewRange(range[0], range[1], humidity2location_map)
+    return min(locationRanges, key=lambda x: x[0])[0]
 
 
 def run1(filename: str):
@@ -101,20 +171,25 @@ def run2(filename: str):
                             map_to_fill.append(
                                 list(map(lambda x: int(x), re.findall("\d+", command)))
                             )
-
+    sortMaps()
     print(
-        min(
-            [
-                getLocationForSeed(seed)
-                for i in range(0, len(seeds), 2)
-                for seed in range(seeds[i], seeds[i] + seeds[i + 1])
-            ]
+        getMinLocationForSeedRange(
+            [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
         )
     )
 
 
 if __name__ == "__main__":
+    # get the start datetime
+    st = datetime.datetime.now()
     filename = "sample.txt"
     # filename = "input.txt"
     # run1(filename)
     run2(filename)
+
+    # get the end datetime
+    et = datetime.datetime.now()
+
+    # get execution time
+    elapsed_time = et - st
+    print("Execution time:", elapsed_time, "seconds")
