fcc002b60f6d787c2262acff9176207b1930041c
fbeclin <60063337+fbeclin@users.noreply.github.com>
2023-12-11 12:50:48 +0100

    part 02

--- 05/run.py
+++ 05/run.py
@@ -67,11 +67,54 @@ def run1(filename: str):
 
 
 def run2(filename: str):
-    pass
+    seeds = []
+    map_to_fill = []
+
+    with open(filename) as file:
+        for line in enumerate(file):
+            if line[0] == 0:
+                seeds = list(
+                    map(
+                        lambda x: int(x),
+                        re.findall("\d+", line[1].rstrip().split(":")[1]),
+                    )
+                )
+            else:
+                command = line[1].rstrip()
+                if command != "":
+                    match command:
+                        case "seed-to-soil map:":
+                            map_to_fill = seed2soil_map
+                        case "soil-to-fertilizer map:":
+                            map_to_fill = soil2fertilizer_map
+                        case "fertilizer-to-water map:":
+                            map_to_fill = fertilizer2water_map
+                        case "water-to-light map:":
+                            map_to_fill = water2light_map
+                        case "light-to-temperature map:":
+                            map_to_fill = light2temperature_map
+                        case "temperature-to-humidity map:":
+                            map_to_fill = temperature2humidity_map
+                        case "humidity-to-location map:":
+                            map_to_fill = humidity2location_map
+                        case _:
+                            map_to_fill.append(
+                                list(map(lambda x: int(x), re.findall("\d+", command)))
+                            )
+
+    print(
+        min(
+            [
+                getLocationForSeed(seed)
+                for i in range(0, len(seeds), 2)
+                for seed in range(seeds[i], seeds[i] + seeds[i + 1])
+            ]
+        )
+    )
 
 
 if __name__ == "__main__":
-    # filename = "sample.txt"
-    filename = "input.txt"
-    run1(filename)
-    # run2(filename)
+    filename = "sample.txt"
+    # filename = "input.txt"
+    # run1(filename)
+    run2(filename)
