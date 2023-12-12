24cf685e48b581acd1b07f25df15af46f1013127
fbeclin <60063337+fbeclin@users.noreply.github.com>
2023-12-13 00:04:42 +0100

    part 2 over

--- 07/run.py
+++ 07/run.py
@@ -26,6 +26,22 @@ card_value = {
     "2": 2,
 }
 
+card_value_with_joker = {
+    "A": 14,
+    "K": 13,
+    "Q": 12,
+    "T": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2,
+    "J": 1,
+}
+
 
 def readHand(handWithBid: []):
     count_dict = {}
@@ -56,6 +72,57 @@ def readHand(handWithBid: []):
         high_card.append(handWithBid)
 
 
+def readHandWithJoker(handWithBid: []):
+    count_dict = {}
+    for card in handWithBid[0]:
+        count = count_dict.get(card)
+        if count is None:
+            count = 0
+        count_dict[card] = count + 1
+
+    nbJoker = count_dict.get("J", 0)
+    nb_same_keys = count_dict.keys()
+    if len(nb_same_keys) == 1:
+        five_of_a_kind.append(handWithBid)
+    elif len(nb_same_keys) == 2:
+        # any joker ?
+        if nbJoker > 0:
+            five_of_a_kind.append(handWithBid)
+        else:
+            # either four or kind or full house
+            if list(count_dict.values())[0] == 1 or list(count_dict.values())[0] == 4:
+                four_of_a_kind.append(handWithBid)
+            else:
+                full_house.append(handWithBid)
+    elif len(nb_same_keys) == 3:
+        # either three of a kind or two pairs
+        if max(count_dict.values()) == 3:
+            # any joker ?
+            if nbJoker > 0:
+                four_of_a_kind.append(handWithBid)
+            else:
+                three_of_a_kind.append(handWithBid)
+        else:
+            if nbJoker == 1:
+                full_house.append(handWithBid)
+            elif nbJoker == 2:
+                four_of_a_kind.append(handWithBid)
+            else:
+                two_pair.append(handWithBid)
+    elif len(nb_same_keys) == 4:
+        # any joker ?
+        if nbJoker > 0:
+            three_of_a_kind.append(handWithBid)
+        else:
+            one_pair.append(handWithBid)
+    else:
+        # any joker ?
+        if nbJoker > 0:
+            one_pair.append(handWithBid)
+        else:
+            high_card.append(handWithBid)
+
+
 def compare(hand_x, hand_y):
     for i, card in enumerate(hand_x[0]):
         compare_to = card_value[card] - card_value[hand_y[0][i]]
@@ -64,21 +131,39 @@ def compare(hand_x, hand_y):
     return 0
 
 
-def sortArrays():
-    five_of_a_kind.sort(key=functools.cmp_to_key(compare))
-    four_of_a_kind.sort(key=functools.cmp_to_key(compare))
-    full_house.sort(key=functools.cmp_to_key(compare))
-    three_of_a_kind.sort(key=functools.cmp_to_key(compare))
-    two_pair.sort(key=functools.cmp_to_key(compare))
-    one_pair.sort(key=functools.cmp_to_key(compare))
-    high_card.sort(key=functools.cmp_to_key(compare))
+def compare_with_joker(hand_x, hand_y):
+    for i, card in enumerate(hand_x[0]):
+        compare_to = card_value_with_joker[card] - card_value_with_joker[hand_y[0][i]]
+        if compare_to != 0:
+            return compare_to
+    return 0
+
+
+def sortArrays(with_joker: bool = False):
+    five_of_a_kind.sort(
+        key=functools.cmp_to_key(compare_with_joker if with_joker else compare)
+    )
+    four_of_a_kind.sort(
+        key=functools.cmp_to_key(compare_with_joker if with_joker else compare)
+    )
+    full_house.sort(
+        key=functools.cmp_to_key(compare_with_joker if with_joker else compare)
+    )
+    three_of_a_kind.sort(
+        key=functools.cmp_to_key(compare_with_joker if with_joker else compare)
+    )
+    two_pair.sort(
+        key=functools.cmp_to_key(compare_with_joker if with_joker else compare)
+    )
+    one_pair.sort(
+        key=functools.cmp_to_key(compare_with_joker if with_joker else compare)
+    )
+    high_card.sort(
+        key=functools.cmp_to_key(compare_with_joker if with_joker else compare)
+    )
 
 
 def computeWiningInArray(card_array: [], startIndex: int):
-    # [
-    #     print(f"{i + 1 + startIndex} * {int(hand[1])}")
-    #     for i, hand in enumerate(card_array)
-    # ]
     return sum(
         [(i + 1 + startIndex) * int(hand[1]) for i, hand in enumerate(card_array)]
     )
@@ -112,12 +197,19 @@ def computeWining():
 
 def debug():
     print(f"five_of_a_kind: {five_of_a_kind}")
+    input()
     print(f"four_of_a_kind: {four_of_a_kind}")
+    input()
     print(f"full_house: {full_house}")
+    input()
     print(f"three_of_a_kind: {three_of_a_kind}")
+    input()
     print(f"two_pair: {two_pair}")
+    input()
     print(f"one_pair: {one_pair}")
+    input()
     print(f"high_card: {high_card}")
+    input()
 
 
 def run1(filename: str):
@@ -134,7 +226,16 @@ def run1(filename: str):
 
 
 def run2(filename: str):
-    pass
+    with open(filename) as file:
+        [readHandWithJoker(line.rstrip().split(" ")) for line in file]
+    # debug()
+
+    # sort arrays
+    sortArrays(with_joker=True)
+    # debug()
+
+    # compute wining
+    computeWining()
 
 
 if __name__ == "__main__":
@@ -142,8 +243,8 @@ if __name__ == "__main__":
     st = datetime.datetime.now()
     # filename = "sample.txt"
     filename = "input.txt"
-    run1(filename)
-    # run2(filename)
+    # run1(filename)
+    run2(filename)
 
     # get the end datetime
     et = datetime.datetime.now()
